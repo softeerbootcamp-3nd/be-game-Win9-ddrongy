3a7cd1d2a711f6eeaff45e9db855b13dc8fe7371
admin <admin@adminui-MacBookPro.local>
2023-12-29 14:39:19 +0900

    [Feat] 카드 더미입력설정

--- src/main/java/softeer/hyundai/domain/Card.java
+++ src/main/java/softeer/hyundai/domain/Card.java
@@ -2,4 +2,8 @@ package softeer.hyundai.domain;
 
 public class Card {
     private String cardType;
+
+    public Card(String cardType) {
+        this.cardType = cardType;
+    }
 }
--- src/main/java/softeer/hyundai/domain/CardDummy.java
+++ src/main/java/softeer/hyundai/domain/CardDummy.java
@@ -5,4 +5,15 @@ import java.util.List;
 
 public class CardDummy {
     private List<Card> cardList;
+
+    public void initializeCards() {
+        List<String> cards = List.of("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J");
+
+        for (int i = 0; i < 4; i++) {
+            for (String cardValue : cards) {
+                cardList.add(new Card(cardValue));
+            }
+        }
+
+    }
 }
