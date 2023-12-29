ea2b15096ee9846407b905af9d891bf05c360da2
admin <admin@adminui-MacBookPro.local>
2023-12-29 14:39:50 +0900

    [Feat] 사용자 금액 입력

--- src/main/java/softeer/hyundai/domain/Player.java
+++ src/main/java/softeer/hyundai/domain/Player.java
@@ -5,4 +5,11 @@ import java.util.List;
 public class Player {
     private int battingMoney;
     private List<Card> cardList;
+
+    public void batMoney(int battingMoney) throws IllegalAccessException {
+        if (battingMoney <= 0) {
+            throw new IllegalAccessException("금액 입력 오류");
+        }
+        this.battingMoney = battingMoney;
+    }
 }
